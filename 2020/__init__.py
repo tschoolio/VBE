ea696fc69035fb8301e1348a3bdcb8bd34e1a45c
maciulaitis <maciulaitis@users.noreply.github.com>
2021-03-26 11:07:09 +0200

    Update __init__.py

--- 2020/__init__.py
+++ 2020/__init__.py
@@ -25,3 +25,13 @@ def emma():
 def blah():
     """responds to word Blah"""
     check50.run("./hello").stdin("Blah").stdout("Blah").exit()
+    
+@check50.check(txtexists)
+def content():
+    with open('test.txt') as f2:
+    linesRez = f2.read().split()
+    if(len(linesRez) < 1):
+        raise check50.Failure("Faile test.txt nepakanka duomenų")
+    else:
+        if (str("testas") != linesRez[0]):
+            raise check50.Failure("Blogi duomenys")   
